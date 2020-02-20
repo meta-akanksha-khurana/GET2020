fef0be3e20976ef93a60493f38ab2926514cff0a
Akanksha Khurana <akanksha.khurana@metacube.com>
2020-02-20 18:39:10 +0530

    EAD Assignment 7(Spring)

---
+++ EAD_Assignment7/src/main/java/com/metacube/mailSender/MailSender.java
@@ -0,0 +1,11 @@
+package com.metacube.mailSender;
+
+/**
+ * This is the interface which represents generic form of different mail senders
+ * @author Akanksha
+ *
+ */
+public interface MailSender {
+
+	void send();
+}
